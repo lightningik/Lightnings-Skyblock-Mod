d7574844955513ced9e1eae05485bc107db52dd5
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2022-09-17 00:38:32 -0700

    Add files via upload

---
+++ src/main/java/me/lightningz/lightningsb/Main.java
@@ -0,0 +1,20 @@
+package me.lightningz.lightningsb;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "lightningsb";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
